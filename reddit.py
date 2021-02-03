ced0b7bbf05ec41e0ef89847def53e72bdd31606
Ganesh Chandrasekaran <g.chandrasekaran@tekaris.com>
2021-02-03 20:27:51 +0100

    crawler script

---
+++ reddit.py
@@ -0,0 +1,38 @@
+#inspired from https://github.com/taivop/joke-dataset
+import praw
+import json
+import time
+import calendar
+
+
+reddit = praw.Reddit("filled with reddit info")
+
+# Enable read-only mode
+reddit.read_only = True
+
+subreddit = reddit.subreddit("jokes")
+
+ts_max = calendar.timegm(time.strptime("2016 09 20","%Y %m %d"))
+
+
+listings = subreddit.submissions(end=ts_max)
+
+
+num_scraped = 0
+save_frequency = 100
+
+jokes = []
+try:
+    for submission in listings:
+        #print("Created: {} [{} UTC]".format(submission.created, submission.created_utc))
+        print("Submission {} [{}] success: [{}] {}".format(num_scraped+1, submission.id, submission.score, submission.title))
+        jokes.append(submission.selftext)
+
+        num_scraped += 1
+
+        if num_scraped % save_frequency == 0:
+            with open("reddit_jokes.txt", "w") as f:
+                json.dump(jokes, f, indent=4, sort_keys=True)
+finally:
+    with open("reddit_jokes.txt", "w") as f:
+        json.dump(jokes, f, indent=4, sort_keys=True)
